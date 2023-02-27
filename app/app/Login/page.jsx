f56920e37fa15c05cd3a7092348faf47eef1ca45
AlexisBroh <aamedinag62@gmail.com>
2023-02-27 13:39:26 -0600

    commit por alexis

---
+++ app/app/Login/page.jsx
@@ -0,0 +1,17 @@
+export default function Home() {
+    return (
+        <>
+        <body>
+            <h1>Login</h1>
+           <label for="email">Email</label>
+        <input type="email" id="email" name="email" />
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" /> 
+        <button type="submit">Login</button>
+        </body>
+        
+            
+       </>
+    
+    )
+}
